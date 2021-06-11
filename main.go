517d2749836aa513266fc1781bd5413166521b94
Manish Meganathan <757manish@gmail.com>
2021-06-12 01:47:48 +0530

    Update main.go for logger initialization
    Added a init() function to initialize the logrus logger

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"os"
+
 	"github.com/manishmeganathan/peerchat/src"
+	"github.com/sirupsen/logrus"
 )
 
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: true,
+	})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logrus.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	logrus.SetLevel(logrus.InfoLevel)
+}
+
 func main() {
+	// username := flag.String("user", "", "username to use in the chatroom.")
+	// chatroom := flag.String("room", "", "chatroom to join.")
+	// flag.Parse()
+
+	// ctx := context.Background()
+
 	ui := src.NewUI()
 	ui.Run()
 }
