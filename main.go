8062c5ff23b0d3fd88399d3e9a44766057ecea62
Manish Meganathan <757manish@gmail.com>
2021-06-14 03:36:25 +0530

    Update main.go
    - Added a welcome figlet
    - Fleshed out full p2p runtime and app start runtime

--- main.go
+++ main.go
@@ -1,34 +1,73 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/manishmeganathan/peerchat/src"
 	"github.com/sirupsen/logrus"
 )
 
+const figlet = `
+
+W E L C O M E  T O
+					     db                  db   
+					     88                  88   
+.8d888b. .d8888b. .d8888b. .d8888b. .d8888b. 88d888b. .d8888b. d8888P 
+88'  '88 88ooood8 88ooood8 88'  '88 88'      88'  '88 88'  '88   88   
+88.  .88 88.      88.      88       88.      88    88 88.  .88   88   
+888888P' '88888P' '88888P' db       '88888P' db    db '8888888   '88P   
+88                                                                    
+dP     
+
+`
+
 func init() {
-	// Log as JSON instead of the default ASCII formatter.
+	// Log as Text with color
 	logrus.SetFormatter(&logrus.TextFormatter{
-		ForceColors:   true,
-		FullTimestamp: true,
+		ForceColors:     true,
+		FullTimestamp:   true,
+		TimestampFormat: time.RFC822,
 	})
 
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
+	// Log to stdout
 	logrus.SetOutput(os.Stdout)
 
-	// Only log the warning severity or above.
+	// Log Info logs and above
 	logrus.SetLevel(logrus.InfoLevel)
 }
 
 func main() {
-	// username := flag.String("user", "", "username to use in the chatroom.")
-	// chatroom := flag.String("room", "", "chatroom to join.")
-	// flag.Parse()
+	// Define input flags
+	username := flag.String("user", "", "username to use in the chatroom.")
+	chatroom := flag.String("room", "", "chatroom to join.")
+	// Parse input flags
+	flag.Parse()
 
-	// ctx := context.Background()
+	// Set background context
+	ctx := context.Background()
 
-	ui := src.NewUI()
+	// Create a new P2PHost
+	p2phost := src.NewP2PHost(ctx)
+	// Bootstrap the DHT
+	p2phost.Bootstrap()
+	// Announce Service CID
+	p2phost.Announce()
+	// Connect to fellow Service CID providers
+	p2phost.Connect()
+
+	// Join the chat room
+	chatapp, _ := src.JoinChatRoom(p2phost, *username, *chatroom)
+
+	// Display the welcome figlet
+	fmt.Print(figlet)
+	time.Sleep(time.Second * 3)
+
+	// Create the Chat UI
+	ui := src.NewUI(chatapp)
+	// Start the UI system
 	ui.Run()
 }
